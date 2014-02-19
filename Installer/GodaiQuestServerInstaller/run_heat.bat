a4559e3ade0aca19c2feeafea77988cee22f40e3
Ohkuma <tkuman@gmail.com>
2014-02-19 18:50:11 +0900

    インストール先のフォルダ名が全部小文字になってしまう問題を修正した

--- Installer/GodaiQuestServerInstaller/run_heat.bat
+++ Installer/GodaiQuestServerInstaller/run_heat.bat
@@ -1,2 +1,2 @@
-heat.exe dir install_data\godaiquestserver -gg -cg "GroupGQS" -dr "TARGETDIR" -var "var.Install_data" -out install_files.wxs
+heat.exe dir install_data\GodaiQuestServer -gg -cg "GroupGQS" -dr "TARGETDIR" -var "var.Install_data" -out install_files.wxs
 pause
