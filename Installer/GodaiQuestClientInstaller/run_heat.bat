dd0038ea4f870e75288daca761a255f72df8c90d
oniprog <tkuman@gmail.com>
2014-08-02 11:42:53 +0900

    セットアップやマニュアルなどを修正した

--- Installer/GodaiQuestClientInstaller/run_heat.bat
+++ Installer/GodaiQuestClientInstaller/run_heat.bat
@@ -1,2 +1,2 @@
-heat.exe dir install_data\GodaiQuestClient -gg -cg "GroupGQC" -dr "ProgramFilesFolder" -var "var.Install_data" -out install_files.wxs
+heat.exe dir install_data\GodaiQuestClient -gg -cg "GroupGQC" -dr "GodaiQuestFolder" -var "var.Install_data" -out install_files.wxs
 pause
